6478634a303ae8023a09f3edf48b0314e669fd35
João Rocha <65980550+Clariosmachine@users.noreply.github.com>
2025-08-20 20:42:57 +0000

    Iniciando variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,46 @@
 //Teste larissa
 
 int main() {
+    //Carta 1:
+    char c1_estado;
+    char c1_codigo[3];
+    char c1_nomeDaCidade[50];
+    int c1_população;
+    float c1_area;
+    int c1_pib;
+    int c1_numedoDePontosTuristicos;
+
+    //Carta 2:
+    char c2_estado;
+    char c2_codigo[3];
+    char c2_nomeDaCidade[50];
+    int c2_população;
+    float c2_area;
+    int c2_pib;
+    int c2_numedoDePontosTuristicos;
+
+    printf("Informe o Estado da Cidade_01 (um caractere de 'A a Z'): \n");
+    scanf("%c", c1_estado);
+    printf("Informe o código da Cidade_01 (O Estado + o número): \n");
+    scanf("%s", c1_codigo);
+    printf("Informe o nome da Cidade_01 (até 50 caracteres): \n");
+    scanf("%s", c1_nomeDaCidade);
+    printf("Digite o número da população da Cidade_01: \n");
+    scanf("%d", &c1_população);
+    printf("Digite a área em km2 da Cidade_01: \n");
+    scanf("%f", &c1_area);
+    printf("Informe o PIB da Cidade_01: \n");
+    scanf("%d", &c1_pib);
+    printf("Digite o número de pontos turísticos da Cidade_01 \n");
+    scanf("%d", &c1_numedoDePontosTuristicos);
+
+    //prints da cidade_01
+    printf("UF %c\n", c1_estado);
+    printf("Código %s\n", c1_codigo);
+    printf("Nome da cidade_01: %s\n", c1_nomeDaCidade);
+
+    
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
